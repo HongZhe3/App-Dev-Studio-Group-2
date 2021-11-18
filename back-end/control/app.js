b73369468ca1b4e75a796238bbb2f3a4c88b92b7
HongZhe3 <hongzhehz.20@ichat.sp.edu.sg>
2021-11-18 15:20:22 +0800

    updates to processing

--- back-end/control/app.js
+++ back-end/control/app.js
@@ -1,15 +1,21 @@
 const express= require('express');
 const post=require('../model/posts');
 var app=express();
+const cors = require('cors');
+app.options('*',cors());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(express.static('public'));
 app.post('/addPost',(req,res)=>{//1
     console.log('\n\ninput',req.body,'\n\n');
     // req.body=JSON.parse(req.body);
-    var title=JSON.parse(req.body.title);
-    var bdy=JSON.parse(req.body.bdy);
-    var uID=JSON.parse(req.body.uID);
-    var cID=JSON.parse(req.body.cID);
-    // var image=req.body.image;
-    post.addPost(title,bdy,uID,cID/* ,image */,(err,result)=>{
+    var title=req.body.title;
+    var bdy=req.body.bdy;
+    var uID=req.body.uID;
+    var cID=req.body.cID;
+    var image=req.body.image;
+    post.addPost(title,bdy,uID,cID,image,(err,result)=>{
         if(err){
             res.status(500).send({'error':err});
         }else{
@@ -18,7 +24,7 @@ app.post('/addPost',(req,res)=>{//1
     });
 });//1
 app.get('/getPosts',(req,res)=>{//2
-    console.log('\n\nget posts',req,'\n\n');
+    console.log('\n\nget posts',req.body,'\n\nparams',req.query);
     post.getPosts((err,result)=>{
         if(err){
             res.status(500).send({'error':err});
