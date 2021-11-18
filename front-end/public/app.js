9f7e87af8d82e3956ae0ac965a8940fa1b32933e
HongZhe3 <hongzhehz.20@ichat.sp.edu.sg>
2021-11-18 15:19:09 +0800

    update location

--- front-end/public/app.js
+++ front-end/public/app.js
@@ -2,10 +2,10 @@ const express=require('express');
 const app=express();
 app.get('/post',(req,res)=>{
     console.log('accessed post');
-    res.sendFile('./public/post.html',{root:__dirname});
+    res.sendFile('post.html',{root:__dirname});
 });
 app.get('*',(req,res)=>{
     console.log('accessed post');
-    res.sendFile('./public/home.html',{root:__dirname});
+    res.sendFile('home.html',{root:__dirname});
 });
 module.exports=app;
