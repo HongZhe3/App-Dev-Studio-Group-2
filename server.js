12e8e0cc3a69d56ee99c0bb37b574567067a7590
HongZhe3 <hongzhehz.20@ichat.sp.edu.sg>
2021-11-18 15:13:28 +0800

    server.js relocation

---
+++ server.js
@@ -0,0 +1,34 @@
+const express=require('express');
+const app=express();
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
+app.get('/post',(req,res)=>{
+    console.log('accessed post');
+    res.sendFile('/public/post.html',{root:__dirname});
+});
+app.get('*',(req,res)=>{
+    console.log('accessed post');
+    res.sendFile('/public/home.html',{root:__dirname});
+});
+// var app=require('./public/app');
+const portA=3000;
+const httpA=require('http');
+const serverA=app.listen(portA,function(){
+    console.log('Web App Hosted at http://localhost:%s',portA);
+});
+
+//back end
+var appB=require('./back-end/control/app');
+const portB =3001;
+const httpB=require('http');
+const serverB=appB.listen(portB,function(){
+    console.log('Web App Hosted at http://localhost:%s',portB);
+});
