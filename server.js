aa09b46b3f5ebd0ad4f0d2332cdbb5b3834c86b5
testtesttesttest45 <weilinquek201@gmail.com>
2021-11-25 16:31:44 +0800

    DEPLOY

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('*',(req,res)=>{
 // var app=require('./public/app');
 const portA=3000;
 const httpA=require('http');
-const serverA=app.listen(portA,function(){
+const serverA=app.listen(portA || process.env.PORT,function(){
     console.log('Web App Hosted at http://localhost:%s',portA);
 });
 
@@ -29,6 +29,6 @@ const serverA=app.listen(portA,function(){
 var appB=require('./back-end/control/app');
 const portB =3001;
 const httpB=require('http');
-appB.listen(portB || process.env.PORT,function(){
+const serverB=appB.listen(portB || process.env.PORT,function(){
     console.log('Web App Hosted at http://localhost:%s',portB);
 });
