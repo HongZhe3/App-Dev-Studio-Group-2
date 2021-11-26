8e18279d93fdb7148e6a2edfea9153ed0dd96ccd
HongZhe3 <hongzhehz.20@ichat.sp.edu.sg>
2021-11-26 09:06:39 +0800

    new endpoints

--- back-end/control/app.js
+++ back-end/control/app.js
@@ -1,5 +1,9 @@
 const express= require('express');
 const post=require('../model/posts');
+const user=require('../model/user');
+const vote=require('../model/vote');
+const channel=require('../model/channel');
+const comment=require('../model/comment');
 var app=express();
 const cors = require('cors');
 app.options('*',cors());
@@ -29,7 +33,7 @@ app.get('/getPosts',(req,res)=>{//2
         if(err){
             res.status(500).send({'error':err});
         }else{
-            res.status(200).send(result);
+            res.status(200).send({'result':result});
         }
     });
 });//2
@@ -48,7 +52,7 @@ app.put('/editPost',(req,res)=>{//3
     });
 });//3
 app.delete('/deletePost',(req,res)=>{//4
-    console.log('\nedit post',req.body,'\n\nparams',req.query);
+    console.log('\ndelete post',req.body,'\n\nparams',req.query);
     var ID=req.query.ID;
     post.deletePost(ID,(err,result)=>{
         if(err){
@@ -61,7 +65,7 @@ app.delete('/deletePost',(req,res)=>{//4
 });//4
 app.get('/getPost',(req,res)=>{//5
     console.log('\nget post',req.body,'\n\nparams',req.query);
-    var ID=req.query.ID;
+    var ID=req.query.id;
     post.getPost(ID,(err,result)=>{
         if(err){
             res.status(500).send({'error':err});
@@ -70,4 +74,39 @@ app.get('/getPost',(req,res)=>{//5
         }
     });
 });//5 test edit
+app.post('/addUser',(req,res)=>{//6
+    console.log('\nadd user',req.body,'\n\nparams',req.query);
+    var username=req.body.username;
+    user.addUser(username,(err,result)=>{
+        if(err){
+            res.status(500).send({'error':err});
+        }else{
+            res.status(201).send(result);
+        }
+    });
+});//6
+app.post('/addChannel',(req,res)=>{//6
+    console.log('\nadd channel',req.body,'\n\nparams',req.query);
+    var name=req.body.name;
+    channel.addCh(name,(err,result)=>{
+        if(err){
+            res.status(500).send({'error':err});
+        }else{
+            res.status(201).send(result);
+        }
+    });
+});//6
+app.post('/votePost',(req,res)=>{
+    console.log('\nvote post',req.body,'\n\nparams',req.query);
+    var uID=req.body.uID;
+    var pID=req.body.pID;
+    var type=req.body.type;
+    vote.vote(uID,pID,type,(err,result)=>{
+        if(err){
+            res.status(500).send({'error':err});
+        }else{
+            res.status(201).send(result);
+        }
+    });
+});
 module.exports=app;
