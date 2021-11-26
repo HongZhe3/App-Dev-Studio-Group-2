9a8b6e6be0f7fb38784ce33130ef6d0d629fde69
HongZhe3 <hongzhehz.20@ichat.sp.edu.sg>
2021-11-26 17:00:10 +0800

    deploy

--- server.js
+++ server.js
@@ -22,14 +22,14 @@ app.get('*',(req,res)=>{
 // var app=require('./public/app');
 const portA=3000;
 const httpA=require('http');
-const serverA=app.listen(portA || process.env.portA,function(){
+const serverA=app.listen(portA || process.env.PORT,function(){
     console.log('Web App Hosted at http://localhost:%s',portA);
 });
 
 //back end
-// var appB=require('./back-end/control/app');
-// const portB =3001;
-// const httpB=require('http');
-// const serverB=appB.listen(portB || process.env.portB,function(){
-//     console.log('Web App Hosted at http://localhost:%s',portB);
-// });
+var appB=require('./back-end/control/app');
+const portB =3001;
+const httpB=require('http');
+const serverB=appB.listen(portB || process.env.PORT,function(){
+    console.log('Web App Hosted at http://localhost:%s',portB);
+});
