7e47af640907c0df550ee1b79ad9c0f3c00b43aa
HongZhe3 <73570972+HongZhe3@users.noreply.github.com>
2021-11-28 11:04:40 +0800

    undo

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 const express=require('express');
 const app=express();
-var appB=require('./back-end/control/app');
-
-app.use('/be',appB);
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
@@ -21,27 +18,17 @@ app.get('*',(req,res)=>{
     console.log('accessed post');
     res.sendFile('./front-end/public/home.html',{root:__dirname});
 });
-
 // var app=require('./public/app');
 const portA=3000;
 const httpA=require('http');
-const serverA=app.listen(process.env.PORT||portA,function(){
-    if(process.env.PORT){
-        //
-        console.log('Web App Hosted at http://localhost:%s',process.env.PORT);
-    }else{
-        console.log('Web App Hosted at http://localhost:%s',portA);
-    }
+const serverA=app.listen(portA || process.env.PORTA,function(){
+    console.log('Web App Hosted at http://localhost:%s',portA);
 });
 
 //back end
-// const portB =3001;
-// const httpB=require('http');
-// const serverB=appB.listen(process.env.PORT||portB,function(){
-//     if(process.env.PORT){
-//         //
-//         console.log('Web App Hosted at http://localhost:%s',process.env.PORT);
-//     }else{
-//         console.log('Web App Hosted at http://localhost:%s',portB);
-//     }
-// });
+var appB=require('./back-end/control/app');
+const portB =3001;
+const httpB=require('http');
+const serverB=appB.listen(portB || process.env.PORTB,function(){
+    console.log('Web App Hosted at http://localhost:%s',portB);
+});
